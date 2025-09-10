bc8569052e17890723abbee3b5520ae8ad931a76
Vinicius <viniciusbsouto@gmail.com>
2025-09-10 14:55:13 -0300

    chore: add poppins font

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
-
 const App = () => {
-  return <h1>Hello, World!</h1>;
+  return <h1 className="text-red-500 p-6 bg-blue-100">Hello, World!</h1>;
 };
 
 export default App;
