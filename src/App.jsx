b8e4b7a81f0d335784c3caceef6abfdced4b0949
Vinicius <viniciusbsouto@gmail.com>
2025-09-15 17:03:13 -0300

    feat: ensure toast is shown after task update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Toaster } from "sonner";
 import Sidebar from "./componentes/Sidebar";
 import Tasks from "./componentes/Tasks";
 
 const App = () => {
   return (
     <div className="flex">
+      <Toaster toastOptions={{ style: { color: "#35383E" } }} />
       <Sidebar />
       <Tasks />
     </div>
--- src/componentes/Tasks.jsx
+++ src/componentes/Tasks.jsx
@@ -8,6 +8,7 @@ import TasksSeparator from "./TasksSeparator";
 import TASKS from "../constants/tasks";
 import { useState } from "react";
 import TaskItem from "./TaskItem";
+import { toast } from "sonner";
 
 const Tasks = () => {
   const [tasks, setTasks] = useState(TASKS);
@@ -19,6 +20,7 @@ const Tasks = () => {
   const handleTaskDeleteClick = (taskId) => {
     const newTasks = tasks.filter((task) => task.id !== taskId);
     setTasks(newTasks);
+    toast.success("Tarefa removida com sucesso!");
   };
 
   const handleTaskCheckboxClick = (taskId) => {
@@ -28,14 +30,17 @@ const Tasks = () => {
       }
 
       if (task.status === "to-do") {
+        toast.success("Tarefa iniciada com sucesso!");
         return { ...task, status: "in-progress" };
       }
 
       if (task.status === "in-progress") {
+        toast.success("Tarefa concluída com sucesso!");
         return { ...task, status: "done" };
       }
 
       if (task.status === "done") {
+        toast.success("Tarefa reiniciada com sucesso!");
         return { ...task, status: "to-do" };
       }
 
